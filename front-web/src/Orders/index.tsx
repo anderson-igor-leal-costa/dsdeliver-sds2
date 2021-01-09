d610486dccce2ecc7ffcb31c46731419ac93c86a
unknown <anderson.igor.leal.costads@gmail.com>
2021-01-09 14:50:17 -0300

    Orders, product card, list, types e api

--- front-web/src/Orders/index.tsx
+++ front-web/src/Orders/index.tsx
@@ -1,13 +1,30 @@
+import ProductsList from './ProductsList';
 import StepsHeader from './StepsHeader';
 import './styles.css'; 
-
+    import {useEffect, useState} from 'react';
+import { Product } from './types';
+import { fetchProducts } from '../api';
 
 function Orders(){
+
+    const [products, setProducts] = useState<Product[]>([]);
+    console.log(products);
+
+    useEffect(() => {
+
+        fetchProducts()
+
+        .then(response => setProducts(response.data))
+        .catch(error => console.log(error))
+
+    }, []); 
+
     return(
         <div>
            <h1 className="orders-container">
                
                <StepsHeader />
+               <ProductsList products={products} />
                
                </h1>
         </div>
