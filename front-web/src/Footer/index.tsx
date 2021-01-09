01e5fcc2404876758e51b13164f4cb6f53aad34a
unknown <anderson.igor.leal.costads@gmail.com>
2021-01-08 23:36:27 -0300

    Add footer

---
+++ front-web/src/Footer/index.tsx
@@ -0,0 +1,39 @@
+import './style.css'; 
+import {ReactComponent as YoutubeIcon} from "./youtube.svg";
+import {ReactComponent as LinkedinIcon} from "./linkedin.svg"
+import {ReactComponent as InstagramIcon} from "./instagram.svg"
+
+function Footer(){
+    return(
+
+            <footer className="main-footer">
+                    App desenvolvido durante a 2ª ed. do evento Semana DevSuperior
+            
+                <div className="footer-icons">
+                <a href="https://www.youtube.com/watch?v=OpWfkIhcBoY&feature=emb_title" target="_new">
+
+                    <YoutubeIcon />
+
+                </a>
+
+                <a href="https://www.figma.com/file/sagWrEqbSarTcpE6Sybgtk/DSDeliver02?node-id=1%3A2" target="_new">
+                <LinkedinIcon />
+
+                </a>
+
+                <a href="https://github.com/devsuperior/sds2/blob/master/aula2/home/styles.css" target="_new">
+                <InstagramIcon />
+
+                </a>
+
+
+                </div>
+            
+            </footer>
+
+            
+
+    )
+}
+
+export default Footer;
--- front-web/src/Home/index.tsx
+++ front-web/src/Home/index.tsx
@@ -1,10 +1,11 @@
 import './styles.css'; 
 import {ReactComponent as MainImage } from './main.svg'; 
+import Footer from '../Footer';
 
 function Home(){
     return(
-
-        <div className="home-container">
+    <>
+     <div className="home-container">
             <div className="home-content">
             <div className="home-actions">
                 <h1 className="home-title">
@@ -28,6 +29,10 @@ function Home(){
 
             </div>
         </div>
+
+        <Footer />
+
+        </>
 
     )
 }
