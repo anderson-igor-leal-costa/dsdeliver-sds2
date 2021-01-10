4cfa183e43b9a48fd2edd88a493abc8a30946b1f
unknown <anderson.igor.leal.costads@gmail.com>
2021-01-10 19:38:56 -0300

    corrigindo o erro

--- front-web/src/Orders/OrderLocation.tsx
+++ front-web/src/Orders/OrderLocation.tsx
@@ -2,6 +2,7 @@ import { useState } from 'react';
 import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
 import AsyncSelect from 'react-select/async'; 
 import { fetchLocalMapBox } from '../api';
+import { OrderLocationData } from './types';
 
 
 const inicialPosition = {
