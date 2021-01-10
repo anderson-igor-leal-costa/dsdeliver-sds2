5bec3e06bebc8c8cbe2ec8f71028570cc2f32db8
unknown <anderson.igor.leal.costads@gmail.com>
2021-01-10 19:00:22 -0300

    Finish front-web

--- front-web/src/App.tsx
+++ front-web/src/App.tsx
@@ -1,3 +1,6 @@
+import { ToastContainer} from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
 import './App.css';
 
 import Routes from './Routes';
@@ -7,9 +10,12 @@ import Routes from './Routes';
 function App() {
   return (
     <div>
-      
-    <Routes />
+      <>
 
+    <Routes />
+    <ToastContainer />
+
+      </>
     </div>
   );
 }
